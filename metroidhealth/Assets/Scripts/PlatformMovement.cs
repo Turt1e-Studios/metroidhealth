29b83c08e6e2b5542945946333e3121bb6f74169
Albert Zhan <albert.cfzhan@gmail.com>
2024-06-18 18:26:25 -0700

    Made a platform

--- metroidhealth/Assets/Scripts/PlatformMovement.cs
+++ metroidhealth/Assets/Scripts/PlatformMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
+    [SerializeField] private Transform[] positions;
+    [SerializeField] private float speed;
+
+    private List<Vector2> _originalPositions;
+    private int _currentPos;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _originalPositions = new List<Vector2>();
+        foreach (Transform position in positions)
+        {
+            _originalPositions.Add(position.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((Vector2) transform.position == _originalPositions[_currentPos])
+        {
+            _currentPos = (_currentPos + 1) % positions.Length;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, _originalPositions[_currentPos], speed * Time.deltaTime);
+        //transform.position = Vector2.Lerp(transform.position, positions[_currentPos % positions.Length], speed * Time.deltaTime);
     }
 }
