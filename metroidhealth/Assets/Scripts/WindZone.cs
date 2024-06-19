ff95cc931896741e8db85eddca54fa29d56f2f6b
Albert Zhan <albert.cfzhan@gmail.com>
2024-06-19 15:47:12 -0700

    wind zone

---
+++ metroidhealth/Assets/Scripts/WindZone.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    [SerializeField] private Vector2 windForce;
+
+    private Collider2D _collider2D;
+    private Collider2D _playerCollider2D;
+    private Rigidbody2D _playerRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider2D = GetComponent<Collider2D>();
+        _playerCollider2D = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+        _playerRigidbody2D = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_collider2D.IsTouching(_playerCollider2D))
+        {
+            _playerRigidbody2D.AddForce(windForce, ForceMode2D.Force);
+        }
+    }
+}
