06ee8ae8c0962a3b8a8b320a7ee0b807ba03fb5b
Albert Zhan <albert.cfzhan@gmail.com>
2024-06-19 15:02:33 -0700

    Added teleporter

---
+++ metroidhealth/Assets/Scripts/PlayerTeleport.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private GameObject _currentTeleporter;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_currentTeleporter != null)
+            {
+                transform.position = _currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            _currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == _currentTeleporter)
+            {
+                _currentTeleporter = null;
+            }
+        }
+    }
+}
