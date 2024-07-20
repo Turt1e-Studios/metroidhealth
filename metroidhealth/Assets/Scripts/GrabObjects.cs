5593cb281904458c4fd15a38a43222dae0cec7b9
ben-6 <ben.yue.hu2@gmail.com>
2024-07-20 13:36:19 -0700

    throw stuff with j key
    press j to throw a block
    change throw force using serializefield property in player -> grab objects

--- metroidhealth/Assets/Scripts/GrabObjects.cs
+++ metroidhealth/Assets/Scripts/GrabObjects.cs
@@ -10,11 +10,13 @@ public class GrabObjects : MonoBehaviour
     [SerializeField] private Transform grabPoint;
     [SerializeField] private Transform rayPoint;
     [SerializeField] private float rayDistance;
+    [SerializeField] private float throwForce;
 
     private GameObject _grabbedObject;
     private PlayerMovement _playerMovement;
     private int _layerIndex;
     private bool _spaceKeyPressed = false;
+    private bool _JKeyPressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,9 @@ public class GrabObjects : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _spaceKeyPressed = true;
+        } else if (Input.GetKeyDown(KeyCode.J))
+        {
+            _JKeyPressed = true;
         }
     }
     void FixedUpdate()
@@ -37,28 +42,44 @@ public class GrabObjects : MonoBehaviour
         RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, direction, rayDistance);
         //print(hitInfo.collider);
         //print(hitInfo.collider.gameObject.layer);
-        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == _layerIndex && _spaceKeyPressed)
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == _layerIndex)
         {
-            
-            // grab object
-            if (_grabbedObject == null)
+            if (_spaceKeyPressed) 
             {
-                _grabbedObject = hitInfo.collider.gameObject;
-                _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                _grabbedObject.transform.position = grabPoint.position;
-                _grabbedObject.transform.SetParent(transform);
-            }
-            // release object
-            else
+                // grab object
+                if (_grabbedObject == null)
+                {
+                    _grabbedObject = hitInfo.collider.gameObject;
+                    _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                    _grabbedObject.transform.position = grabPoint.position;
+                    _grabbedObject.transform.SetParent(transform);
+                }
+                // release object
+                else
+                {
+                    _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                    _grabbedObject.transform.SetParent(null);
+                    _grabbedObject = null;
+                }
+                _spaceKeyPressed = false;
+            } else if (_JKeyPressed)
             {
-                _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                _grabbedObject.transform.SetParent(null);
-                _grabbedObject = null;
+                if (_grabbedObject != null)
+                {
+                    // throw with velocity
+                    _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                    _grabbedObject.transform.SetParent(null);
+                    _grabbedObject.GetComponent<Rigidbody2D>().AddForce(direction * throwForce, ForceMode2D.Impulse);
+                    _grabbedObject = null;
+                     
+                }
+                
+                _JKeyPressed = false;
             }
-            
+
         }
 
-        _spaceKeyPressed = false;
-        Debug.DrawRay(rayPoint.position, direction * rayDistance);
+        
+        //Debug.DrawRay(rayPoint.position, direction * rayDistance);
     }
 }
